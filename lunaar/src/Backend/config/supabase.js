92ed04914e676ceaa6b32623517e1769d7ab1b9f
rutu48836-create <rutu48836@gmail.com>
2026-05-28 12:33:46 +0530

    Update supabase.js

--- lunaar/src/Backend/config/supabase.js
+++ lunaar/src/Backend/config/supabase.js
@@ -4,9 +4,6 @@ import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: resolve(__dirname, '../.env') });
-
 export const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY,
