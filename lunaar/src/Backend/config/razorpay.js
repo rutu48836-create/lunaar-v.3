4ef0b3672d5223fcd2df7a63651abfae6326532b
rutu48836-create <rutu48836@gmail.com>
2026-05-28 12:34:00 +0530

    Update razorpay.js

--- lunaar/src/Backend/config/razorpay.js
+++ lunaar/src/Backend/config/razorpay.js
@@ -4,9 +4,6 @@ import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
 import dotenv from "dotenv";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: resolve(__dirname, '../.env') });
-
 export const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
